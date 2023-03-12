7268dd85d73dd3bd905dcf75f2b72462dae515a1
Ben Muschol <benmuschol@Bens-Air.lan>
2023-03-11 22:38:47 -0500

    Controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'games/index'
+  get 'games/show'
+  resources :games, only: [:index, :show]
 end
